edddcafa2a94de9ef56053a9d79af812093c850a
Shiny <stefanie.eifert@yahoo.com>
2019-02-16 12:25:32 -0500

    added basic html, added express

---
+++ server.js
@@ -0,0 +1,28 @@
+// Node Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// Link in html and api routes
+var apiRoutes = require('./app/routing/apiRoutes.js');
+var htmlRoutes = require('./app/routing/htmlRoutes.js');
+
+// Set up Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// Server Routing Map 
+apiRoutes(app); // API route - Must be listed first due to the HTML default catch all "use" route
+htmlRoutes(app); // HTML route 
+
+// Listener - Start the server
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
