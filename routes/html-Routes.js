a51d1793d88e57102f5b1af26578bcc81a38b69d
Shiny <stefanie.eifert@yahoo.com>
2019-02-24 20:13:04 -0500

    updated html-routes.js file

--- routes/html-Routes.js
+++ routes/html-Routes.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = function(app) {
   // Below code handles when users 'visit' a page.
   app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
   });
   // Default home route
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
   });
 };
